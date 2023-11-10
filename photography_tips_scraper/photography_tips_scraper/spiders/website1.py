7a5315d7d7d3827801003c8c7ee51f91dc4f56bb
Nicolo Tafta <venditasetam@gmail.com>
2023-11-10 22:17:01 +0100

    feature: Changed the type of scraping:
    - Added functions for scrape from each category of website.
    
    - Added function for scrape each article and not just the preview as before.
    
    Co-authored-by: mine, I and me

--- photography_tips_scraper/photography_tips_scraper/spiders/website1.py
+++ photography_tips_scraper/photography_tips_scraper/spiders/website1.py
@@ -1,7 +1,5 @@
-from pathlib import Path
-from urllib.parse import urlencode
-
 import scrapy
+from urllib.parse import urlencode
 
 API_KEY = "1208de1b-9d3a-435f-a41c-df151ef7cfd1"
 
@@ -15,31 +13,72 @@ def get_scrapeops_url(url):
 class Website1Spider(scrapy.Spider):
     name = "website1"
     allowed_domains = ["www.dpreview.com"]
-    start_urls = [
-        "https://www.dpreview.com/features/photography?page=1",
-    ]
+    # start_urls = ["https://www.dpreview.com/features/photography"]
+
+    start_url = 'https://www.dpreview.com/features'
+
+    def parse_start_urls(self, response):
+        # Find all the urls of each section of articles
+        sections = response.css('div.sampleArticles')
+
+        for section in sections:
+            url = section.css('div.category div.articlesCount a::attr(href)').get()
+            section_url = response.urljoin(url)
+            headers = {'User-Agent': 'UserAgent'}
+            # go inside the url of the article
+            yield scrapy.Request(url=section_url, headers=headers, callback=self.parse)
 
     def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url=get_scrapeops_url(url), callback=self.parse)
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+        # yield scrapy.Request(url=get_scrapeops_url(self.start_urls[0]), callback=self.parse)
+        yield scrapy.Request(url=self.start_url, headers=headers, callback=self.parse_start_urls)
+
+    def parse_article(self, response):
+        # Extract the title from the current article
+        title = response.css('div.articleHeader h1::text').get()
+
+        # Extract the content from the current article
+        main_content = response.css('div.mainContent div.articleBody')
+
+        content = main_content.xpath('.//p/text() | .//h2/text()').getall()
+        data = {
+            'title': title,
+            'content': content,
+            'url': response.url,
+        }
+
+        # Yield the data for this article
+        yield data
 
     def parse(self, response):
         # Find all article divs
         articles = response.css('div.article')
 
         for article in articles:
-            # Extract the title from the current article
-            title = article.css('div.content div.header div.title a::text').get()
 
-            # Extract the content from the current article
-            content = article.css('div.content div.body p::text').get()
+            url = article.css('div.content div.header div.title a::attr(href)').get()
+            # output the url of the article
 
-            # Create a dictionary to store the extracted data for this article
-            data = {
-                'title': title,
-                'content': content,
-                'url': response.url,
-            }
+            article_url = response.urljoin(url)
+            headers = {'User-Agent': 'UserAgent'}
+            # go inside the url of the article
+            yield scrapy.Request(url=article_url, headers=headers, callback=self.parse_article)
 
-            # Yield the data for this article
-            yield data
+
+        # Extract the next page URL
+        next_page = response.css('td.next.enabled a::attr(href)').get()
+
+        # If there's a next page, follow the link and continue scraping
+        if next_page:
+            # Construct the absolute URL without involving the ScrapeOps proxy
+            next_page_url = response.urljoin(next_page)
+            headers = {'User-Agent': 'UserAgent'}
+            yield scrapy.Request(url=next_page_url, headers=headers, callback=self.parse)
+
+
+
+
+
+# fix link in the articles that are not linked well
+# fix the regex for the p for get the text clean and get the text from all the tags inside (a, b, strong, etc)
+
