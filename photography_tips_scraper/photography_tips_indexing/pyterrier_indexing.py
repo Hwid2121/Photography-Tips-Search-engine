b081afc901b6578fdcc540dd79ea4d14b90c6cb7
Nicolo Tafta <venditasetam@gmail.com>
2023-11-30 11:38:06 +0100

    updated project

--- photography_tips_scraper/photography_tips_indexing/pyterrier_indexing.py
+++ photography_tips_scraper/photography_tips_indexing/pyterrier_indexing.py
@@ -11,8 +11,10 @@ docs_df = db.docs_df
 index_path = "./pd_index"
 
 # Specify overwrite=True when creating the DFIndexer
-pd_indexer = pt.DFIndexer(index_path, overwrite=True, verbose=True)
-index_ref = pd_indexer.index(text=docs_df['content'], docno=docs_df['_id'])
+pd_indexer = pt.DFIndexer(index_path, overwrite=True, verbose=False)
+index_ref = pd_indexer.index(
+    text=docs_df['content']
+, docno=docs_df['_id'])
 # Optionally, print information about the index
 index = pt.IndexFactory.of(index_ref)
 
@@ -21,8 +23,36 @@ print(index.getCollectionStatistics().toString())
 # Create BatchRetrieve
 br = pt.BatchRetrieve(index, wmodel="Tf")
 
-# Perform a search
 query = "photo editing software"
-br.search(query)
+query = [[str(i+1), e] for i, e in enumerate(query.split(" "))]
+topics = pd.DataFrame(query, columns=["qid", "query"])
+res = br.transform(topics)
 
-# br.transform(query)
+final_result = docs_df[docs_df["_id"].isin(res["docno"])]
+# final_result_title = final_result[["title"]]
+# final_result contine cio che vuoi
+
+print(res)
+print(final_result)
+res.to_csv("res.csv")
+final_result.to_csv("final_result.csv")
+
+# Perform a search
+# query = "photo editing software"
+# br.search(query)
+
+# Install pyenv
+# pyenv virtualenv name_of_venv
+# source ... activate venv
+# VS code python select interpreter
+# requirements.txt
+# pip install -r requirements.txt
+# Controllare il VENV: pip -V
+
+
+# Scrapy==2.11.0
+# python-terrier==0.10.0
+# pandas==2.0.3
+
+
+# pip freeze | grep name_of_package
